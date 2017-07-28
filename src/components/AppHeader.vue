2b5e5777b1f3bd953b26437ef7a7c15d6a109eb3
Cameron <cameron@biteable.com>
2017-07-29 09:36:50 +1000

    better logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="AppHeader">
-    <svg class="AppHeader__Logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="#499aff" d="M43 11c0-1.105-.895-2-2-2H6C4.346 9 3 7.654 3 6c0-.552-.448-1-1-1s-1 .448-1 1v32c0 3.866 3.134 7 7 7h33c1.105 0 2-.895 2-2V11z"/><path fill="#397ED5" d="M46 22H36c-2.757 0-5 2.243-5 5s2.243 5 5 5h10c.552 0 1-.448 1-1v-8c0-.552-.448-1-1-1zM4 3h28c1.105 0 2 .895 2 2v4H4C2.343 9 1 7.657 1 6s1.343-3 3-3z"/><circle cx="36" cy="27" r="2" fill="#fff"/></svg>
+    <svg class="AppHeader__Logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><g fill="none" fill-rule="evenodd"><path fill="#499AFF" d="M42 8c0-1.105-.895-2-2-2H5C3.346 6 2 4.654 2 3c0-.552-.448-1-1-1s-1 .448-1 1v32c0 3.866 3.134 7 7 7h33c1.105 0 2-.895 2-2V8z"/><path fill="#3F84D9" d="M3 0h28c1.105 0 2 .895 2 2v4H3C1.343 6 0 4.657 0 3s1.343-3 3-3z"/><path fill="#FFF" d="M36.414 22.586c.78.78.78 2.047 0 2.828-.78.78-2.047.78-2.828 0-.78-.78-.78-2.047 0-2.828.78-.78 2.047-.78 2.828 0"/></g></svg>
     <h1 class="AppHeader__Heading">PoorScore</h1>
   </header>
 </template>
@@ -21,8 +21,9 @@ export default {};
   }
 
   &__Logo {
-    width: 3.5rem;
-    height: 3.5rem;
+    width: 3rem;
+    height: 3rem;
+    margin-bottom: 0.5rem;
   }
 }
 </style>
